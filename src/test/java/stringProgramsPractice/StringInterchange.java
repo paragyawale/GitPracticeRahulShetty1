8a7ae84afeb7548ff47f452342da126c05f5508b
parag <yawale.parag@gmail.com>
2022-08-30 18:31:32 +0530

    from cloneFolder

---
+++ src/test/java/stringProgramsPractice/StringInterchange.java
@@ -0,0 +1,13 @@
+package stringProgramsPractice;
+
+public class StringInterchange {
+
+	public static void main(String[] args) {
+		
+		String s = "MY NAME IS PARAG";
+		// output = YM EMAN SI GARAP"
+	
+
+	}
+
+}
