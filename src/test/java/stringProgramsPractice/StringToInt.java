4ab06d1d3f1ab80cff07c8568482716db1d88c7e
parag <yawale.parag@gmail.com>
2022-08-30 18:07:38 +0530

    new branch commit

---
+++ src/test/java/stringProgramsPractice/StringToInt.java
@@ -0,0 +1,14 @@
+package stringProgramsPractice;
+
+public class StringToInt {
+
+	public static void main (String [] args) {
+		
+		String s = "9";
+		
+		int n = Integer.parseInt(s);
+		System.out.println(n+1);
+		
+		
+	}
+}
